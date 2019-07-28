cbc19b1a0d4be74b6016ae427c940341381ab2ee
romanautomation <romanraintom@gmail.com>
2019-07-28 21:22:01 +0530

    first git commit for my selenium code

---
+++ src/test/java/test2/DemoTest.java
@@ -0,0 +1,45 @@
+package test2;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test(priority=0)
+	public void sum(){
+		System.out.println("SUM");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(30, a+b);
+	}
+	
+	
+	@Test(priority=3)
+	public void sub(){
+		System.out.println("SUBSTRACT");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(10, b-a);
+	}
+	
+	
+	@Test(priority=1)
+	public void mul(){
+		System.out.println("MULTIPLY");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(200, a*b);
+		
+		
+	}
+	
+	@Test(priority=2)
+	public void div(){
+		System.out.println("DIVIDE");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(2, b/a);
+		
+		
+	}
+}
